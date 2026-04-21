496a16a6b971fce6f0df930b8b3306d16d65daad
Can <cantpts@outlook.de>
2026-04-22 01:32:20 +0200

    feat: add close button on create-contact modal

--- src/app/components/contact-create-modal/contact-create-modal.ts
+++ src/app/components/contact-create-modal/contact-create-modal.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core';
+import { DialogRef } from '@angular/cdk/dialog';
+import { Component, inject } from '@angular/core';
 
 @Component({
-  selector: 'app-contact-create-modal',
-  imports: [],
-  templateUrl: './contact-create-modal.html',
-  styleUrl: './contact-create-modal.scss',
+	selector: 'app-contact-create-modal',
+	imports: [],
+	templateUrl: './contact-create-modal.html',
+	styleUrl: './contact-create-modal.scss',
 })
-export class ContactCreateModal {}
+export class ContactCreateModal {
+	private dialogRef = inject(DialogRef);
+	protected closeModal() {
+		this.dialogRef?.close();
+	}
+}
