c9eb756a5151dbb53dd045be2dc11f5390e2e2c8
Can <cantpts@outlook.de>
2026-04-27 20:50:59 +0200

    fix: dialog validation requirements and design fixxes

--- src/app/components/contact-create-modal/contact-create-modal.ts
+++ src/app/components/contact-create-modal/contact-create-modal.ts
@@ -1,103 +1,84 @@
-import { DialogRef } from '@angular/cdk/dialog';
+import { DIALOG_DATA, DialogRef } from '@angular/cdk/dialog';
 import { Component, inject, signal } from '@angular/core';
-import {
-  FormControl,
-  FormsModule,
-  Validators,
-  ReactiveFormsModule,
-  FormGroup,
-} from '@angular/forms';
+import {FormControl,Validators,ReactiveFormsModule,} from '@angular/forms';
+import { Contact } from '../../models/contact';
 import { ContactsService } from '../../services/contacts';
 import { ToastService } from '../../services/toast';
 
 @Component({
-  selector: 'app-contact-create-modal',
-  imports: [FormsModule, ReactiveFormsModule],
-  templateUrl: './contact-create-modal.html',
-  styleUrl: './contact-create-modal.scss',
+	selector: 'app-contact-create-modal',
+	imports: [ReactiveFormsModule],
+	templateUrl: './contact-create-modal.html',
+	styleUrl: './contact-create-modal.scss',
 })
 export class ContactCreateModal {
-  private dialogRef = inject(DialogRef);
-  private contactsService = inject(ContactsService);
-  private toastService = inject(ToastService);
-  // protected name = '';
-  // protected email = '';
-  // protected phone = '';
-  protected saving = signal(false);
-  protected errorMessage = signal<string | null>(null);
+	private dialogRef = inject(DialogRef);
+	private contactsService = inject(ContactsService);
+	private toastService = inject(ToastService);
 
-  contactForm = new FormGroup({
-    name: new FormControl('', {
-      validators: [
-        Validators.required,
-        Validators.pattern(/^[a-zA-Zà-žÀ-Ž-]+ +[a-zA-Zà-žÀ-Ž-]+.*$/),
-      ],
-    }),
+	protected saving = signal(false);
+	protected errorMessage = signal<string | null>(null);
 
-    email: new FormControl('', {
-      validators: [
-        Validators.required,
-        Validators.pattern('[A-Za-z0-9._%-]+@[A-Za-z0-9._%-]+\\.[a-z]{2,3}'),
-      ],
-    }),
-    phone: new FormControl('', {
-      validators: [Validators.required, Validators.pattern('[+ 0-9 ]{11,13}')],
-    }),
-  });
+	name = new FormControl(''.trim(), {
+		validators: [Validators.required, Validators.minLength(3), Validators.pattern(/^[a-zA-Zà-žÀ-Ž]{2,} +[a-zA-Zà-žÀ-Ž]{2,}$/)],
+		nonNullable: true
+	});
 
-  protected async createContact() {
-    this.errorMessage.set('');
+	email = new FormControl('', {
+		validators: [Validators.required, Validators.pattern('[A-Za-z0-9._%-]+@[A-Za-z0-9._%-]+\\.[a-z]{2,3}')],
+		nonNullable: true
+	})
 
-    let first_name;
-    let last_name;
-    if (this.contactForm.value.name) {
-      const full = this.contactForm.value.name.trim();
-      const space = full.indexOf(' ');
-      first_name = space === -1 ? full : full.slice(0, space);
-      last_name = space === -1 ? '' : full.slice(space + 1).trim();
-    }
+	phone = new FormControl('', {
+		validators: [Validators.required, Validators.pattern(/^\+?[0-9 ]{10,12}$/)],
+		nonNullable: true
+	})
 
-    if (
-      !first_name ||
-      !last_name ||
-      !this.contactForm.value.email?.trim() ||
-      !this.contactForm.value.phone
-    ) {
-      this.errorMessage.set('Name, email and phone are required.');
-      return;
-    }
-    this.saving.set(true);
-    try {
-      await this.contactsService.createContact({
-        first_name,
-        last_name,
-        email: this.contactForm.value.email,
-        phone: this.contactForm.value.phone,
-      });
-      this.toastService.show('Contact created successfully', 2500);
-      this.closeModal(true);
-    } catch (error) {
-      const message =
-        error instanceof Error && error.message ? error.message : 'Could not save contact.';
-      this.errorMessage.set(message);
-    } finally {
-      this.saving.set(false);
-    }
-  }
+	protected async createContact() {
+		this.errorMessage.set('');
 
-  protected closeModal(result: boolean = false) {
-    this.dialogRef?.close(result);
-  }
+		// error validation
+		if (this.name.invalid || this.email.invalid || this.phone.invalid) {
+			this.name.markAsTouched();
+			this.email.markAsTouched();
+			this.phone.markAsTouched();
+			this.errorMessage.set('Please fix the errors');
+			return;
+		}
 
-  get name() {
-    return this.contactForm.get('name');
-  }
+		// werte aus inputs extrahieren
+		// value = aktueller wert aus input
+		const full = this.name.value.trim();
+		const emailValue = this.email.value.trim();
+		const phoneValue = this.phone.value.trim();
 
-  get email() {
-    return this.contactForm.get('email');
-  }
+		// splitting name
+		const space = full.indexOf(' ');
+		const first_name = space === -1 ? full : full.slice(0, space);
+		const last_name = space === -1 ? '' : full.slice(space + 1).trim();
 
-  get phone() {
-    return this.contactForm.get('phone');
-  }
+		this.saving.set(true);
+
+		try {
+			await this.contactsService.createContact({
+				first_name,
+                last_name,
+                email: emailValue,
+                phone: phoneValue,
+			});
+
+			this.toastService.show('Contact created successfully', 2500);
+			this.closeModal(true);
+		} catch (error) {
+			const message =
+				error instanceof Error && error.message ? error.message : 'Could not save contact.';
+			this.errorMessage.set(message);
+		} finally {
+			this.saving.set(false);
+		}
+	}
+
+	protected closeModal(result: boolean = false) {
+		this.dialogRef?.close(result);
+	}
 }
