0f122eb0cf2de900f095e9b5ec97d7b9ccb7c45f
Reza <behruz.mreza@gmail.com>
2026-04-30 21:28:05 +0200

    feat: add task Card layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { ContactList } from './components/contact-list/contact-list';
+import { Task } from './components/task/task';
 
 export const routes: Routes = [
   { path: 'contacts', component: ContactList },
+  { path: 'task', component: Task },
 ];
