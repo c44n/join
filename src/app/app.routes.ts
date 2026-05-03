9ced207f040d80e6cea9244f5bcdaf9f1e16fd2d
Md Atahar Hossain <mdataharhossain@Mac.fritz.box>
2026-05-03 11:19:57 +0200

    feat: Add add-task-component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
+import { AddTask } from './components/add-task/add-task';
 import { ContactList } from './components/contact-list/contact-list';
 
 export const routes: Routes = [
+  { path: 'add-task', component: AddTask },
   { path: 'contacts', component: ContactList },
 ];
