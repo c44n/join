19e69b7bf993a3eea178b32e7c1ca03d42804841
Can <cantpts@outlook.de>
2026-05-04 08:57:59 +0200

    feat: add board component, board body design

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { ContactList } from './components/contact-list/contact-list';
+import { Board } from './components/board/board';
 
 export const routes: Routes = [
   { path: 'contacts', component: ContactList },
+  { path: 'board', component: Board },
 ];
