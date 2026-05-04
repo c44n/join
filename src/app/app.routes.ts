6c8e585bffa87dc324f66313edd436c23a4e38f1
Can <cantpts@outlook.de>
2026-05-04 11:50:34 +0200

    feat: add board-header to board layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { AddTask } from './components/add-task/add-task';
 import { ContactList } from './components/contact-list/contact-list';
 import { Board } from './components/board/board';
+import { BoardHeader } from './components/board-header/board-header';
 
 export const routes: Routes = [
   { path: 'add-task', component: AddTask },
   { path: 'contacts', component: ContactList },
   { path: 'board', component: Board },
+  { path: 'board_header', component: BoardHeader },
 ];
