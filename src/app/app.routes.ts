5ad86171c1d47135683f91cbf23292255a4a7364
Can <cantpts@outlook.de>
2026-05-04 12:02:20 +0200

    add: add task card design to board

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-import { Routes } from '@angular/router';
-import { AddTask } from './components/add-task/add-task';
-import { ContactList } from './components/contact-list/contact-list';
-import { Board } from './components/board/board';
-import { BoardHeader } from './components/board-header/board-header';
-
-export const routes: Routes = [
-  { path: 'add-task', component: AddTask },
-  { path: 'contacts', component: ContactList },
-  { path: 'board', component: Board },
-  { path: 'board_header', component: BoardHeader },
-];
-=======
 import { Routes } from '@angular/router';
 import { AddTask } from './components/add-task/add-task';
 import { ContactList } from './components/contact-list/contact-list';
@@ -22,4 +8,3 @@ export const routes: Routes = [
   { path: 'contacts', component: ContactList },
   { path: 'board', component: Board },
 ];
->>>>>>> ad5817e48233f7b6412bbaaafe479dd3cff6d705
--- src/app/components/board/board.ts
+++ src/app/components/board/board.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { CdkMenu, CdkMenuItem, CdkMenuTrigger } from '@angular/cdk/menu';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+
 
 @Component({
   selector: 'app-board',
-  imports: [],
+  imports: [MatProgressBarModule, CdkMenuTrigger, CdkMenu, CdkMenuItem],
   templateUrl: './board.html',
   styleUrl: './board.scss',
 })
